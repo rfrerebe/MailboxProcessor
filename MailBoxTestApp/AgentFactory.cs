d75b764cf275d0aabf1bc50a00499389e4ed40c1
Maxim T <lptscsi@gmail.com>
2021-01-04 22:58:30 +0300

    Test App update

---
+++ MailBoxTestApp/AgentFactory.cs
@@ -0,0 +1,73 @@
+﻿using MailboxProcessor;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace MailBoxTestApp
+{
+    public static class AgentFactory
+    {
+        public static Agent<Message> GetAgent(string filePath, CancellationToken? token = null, int? capacity = null)
+        {
+            var agent = new Agent<Message>(async inbox =>
+            {
+                int n = 0;
+
+                // int threadId = Thread.CurrentThread.ManagedThreadId;
+
+                using var fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.Read, 4 * 1024, false);
+                using var streamWriter = new StreamWriter(fileStream, System.Text.Encoding.UTF8, 4096, true);
+
+                Console.WriteLine("Starting MailboxProcessor");
+
+                while (!inbox.CancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var msg = await inbox.Receive();
+
+                        if (msg is AddLineMessage addLineMessage)
+                        {
+                            streamWriter.WriteLine(addLineMessage.Line);
+                            ++n;
+                        }
+                        else if (msg is Reset)
+                        {
+                            n = 0;
+                        }
+                        else if (msg is AddLineAndReplyMessage addLineandReplyMessage)
+                        {
+                            streamWriter.WriteLine(addLineandReplyMessage.Line);
+                            ++n;
+                            var chan = addLineandReplyMessage.Channel;
+                            chan.Reply(n);
+                        }
+                        else if (msg is AddMultyLineMessage addMultyLineMessage)
+                        {
+                            streamWriter.WriteLine(addMultyLineMessage.Line);
+                            ++n;
+                            for (int k = 0; k < 5; ++k)
+                            {
+                                string line4 = $"{n}-{k}) {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
+                                await addMultyLineMessage.NextAgent.Post(new AddLineMessage(line4));
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        inbox.ReportError(ex);
+                    }
+                }
+
+                streamWriter.Flush();
+
+                Console.WriteLine("Exiting MailboxProcessor");
+            }, token, capacity);
+
+            agent.Start();
+
+            return agent;
+        }
+    }
+}
---
+++ MailBoxTestApp/Messages.cs
@@ -0,0 +1,48 @@
+﻿using MailboxProcessor;
+
+namespace MailBoxTestApp
+{
+    public class Message
+    {
+    }
+
+    public class Reset : Message
+    {
+    }
+
+    public class AddLineMessage : Message
+    {
+        public AddLineMessage(string line)
+        {
+            this.Line = line;
+        }
+
+        public string Line { get; private set; }
+    }
+
+    public class AddMultyLineMessage : Message
+    {
+        public AddMultyLineMessage(string line, Agent<Message> nextAgent)
+        {
+            this.Line = line;
+            this.NextAgent = nextAgent;
+        }
+
+        public string Line { get; private set; }
+
+        public Agent<Message> NextAgent { get; private set; }
+    }
+
+    public class AddLineAndReplyMessage : Message
+    {
+        public AddLineAndReplyMessage(IReplyChannel<int?> channel, string line)
+        {
+            this.Channel = channel;
+            this.Line = line;
+        }
+
+        public IReplyChannel<int?> Channel { get; private set; }
+
+        public string Line { get; private set; }
+    }
+}
