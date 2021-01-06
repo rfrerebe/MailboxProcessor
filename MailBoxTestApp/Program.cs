1dc09112f04ef27fc08db2f8d080d2ba0a2934c5
Maxim T <lptscsi@gmail.com>
2021-01-06 03:52:11 +0300

    Update Program.cs
    Making TestApp more interesting, to check using the same agent in several threads at once

--- MailBoxTestApp/Program.cs
+++ MailBoxTestApp/Program.cs
@@ -16,35 +16,47 @@ namespace MailBoxTestApp
 
             async Task RunJob(Agent<Message> agent1, Agent<Message> agent2, Agent<Message> agent3, Agent<Message> agent4)
             {
-                // Console.WriteLine($"Agent Work Thread { Thread.CurrentThread.ManagedThreadId}");
+                const int numberOfLines = 25000;
                 try
                 {
-                    for (int i = 0; i < 25000; ++i)
+                    // task #1 which uses 1 agent to post lines
+                    Func<Task> taskPart1 = async ()=>
                     {
-                        string line = $"Line1 {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
-                        var lineNumber = await agent1.PostAndReply<int?>(channel => new AddLineAndReplyMessage(channel, line));
-
-                        for (int j = 0; j < 5; ++j)
+                        for (int i = 0; i < numberOfLines; ++i)
                         {
-                            string line2 = $"Line2 {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
-                            // wait for reply
-                            var reply = await agent2.PostAndReply<AddMultyLineMessageReply>(channel => new AddMultyLineMessage(channel, line: line2));
-                            
-                            // send all lines in reply to agent4
-                            foreach(var line4 in reply.Lines)
+                            for (int j = 0; j < 5; ++j)
                             {
-                                await agent4.Post(new AddLineMessage(line4));
+                                string line3 = $"Line3 {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
+                                await agent3.Post(new AddLineMessage(line3));
                             }
                         }
+                    };
 
-                        for (int j = 0; j < 5; ++j)
+                    // task #2 which uses several agents to post lines and get replies
+                    Func<Task> taskPart2 = async () =>
+                    {
+                        for (int i = 0; i < numberOfLines; ++i)
                         {
-                            string line3 = $"Line3 {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
-                            await agent3.Post(new AddLineMessage(line3));
+                            string line = $"Line1 {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
+                            var lineNumber = await agent1.PostAndReply<int?>(channel => new AddLineAndReplyMessage(channel, line));
+
+                            for (int j = 0; j < 5; ++j)
+                            {
+                                string line2 = $"Line2 {string.Concat(Enumerable.Repeat(Guid.NewGuid().ToString(), 25))}";
+                                // wait for reply
+                                var reply = await agent2.PostAndReply<AddMultyLineMessageReply>(channel => new AddMultyLineMessage(channel, line: line2));
+
+                                // send all lines in reply to agent4
+                                foreach (var line4 in reply.Lines)
+                                {
+                                    await agent4.Post(new AddLineMessage(line4));
+                                }
+                            }
                         }
-                        
-                        // Console.WriteLine($"Agent Work Thread {Thread.CurrentThread.ManagedThreadId}");
-                    }
+                    };
+
+                    // several tasks are run in parrallel
+                    await Task.WhenAll(Task.Run(taskPart1), Task.Run(taskPart1), Task.Run(taskPart1), Task.Run(taskPart2));
                 }
                 catch (OperationCanceledException)
                 {
@@ -52,10 +64,12 @@ namespace MailBoxTestApp
                 }
             };
 
-            using (var agent1 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox1.txt", token: cts.Token, capacity: 100))
-            using (var agent2 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox2.txt", token: cts.Token, capacity: 100))
-            using (var agent3 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox3.txt", token: cts.Token, capacity: 100))
-            using (var agent4 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox4.txt", token: cts.Token, capacity: 100))
+            const int capacity = 100;
+
+            using (var agent1 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox1.txt", token: cts.Token, capacity: capacity))
+            using (var agent2 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox2.txt", token: cts.Token, capacity: capacity))
+            using (var agent3 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox3.txt", token: cts.Token, capacity: capacity))
+            using (var agent4 = AgentFactory.GetAgent(filePath: @"c:\TEMP\testMailbox4.txt", token: cts.Token, capacity: capacity))
             {
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
