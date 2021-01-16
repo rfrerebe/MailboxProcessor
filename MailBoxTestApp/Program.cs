f937d680012634d25a70e7e8ed1bd9987d15fd14
Maxim T <lptscsi@gmail.com>
2021-01-16 14:08:04 +0300

    Project update

--- MailBoxTestApp/Program.cs
+++ MailBoxTestApp/Program.cs
@@ -14,50 +14,7 @@ namespace MailBoxTestApp
             // cts.CancelAfter(100);
 
             AgentOptions agentOptions = new AgentOptions() { CancellationToken= cts.Token, BoundedCapacity= 10 };
-
-            /*
-            using var scheduler = new Threading.Schedulers.WorkStealingTaskScheduler();
-
-            var t1 = Task.Factory.StartNew(async () => {
-                // Starting on theadpool thread here
-                Console.WriteLine($"Thread1: {Thread.CurrentThread.ManagedThreadId} IsPool: {Thread.CurrentThread.IsThreadPoolThread}");
-                await Task.Delay(100);
-                // Still on theadpool thread here
-                Console.WriteLine($"Thread2: {Thread.CurrentThread.ManagedThreadId} IsPool: {Thread.CurrentThread.IsThreadPoolThread}");
-
-                await Task.CompletedTask.ConfigureLongRunning();
-                Console.WriteLine($"Thread3: {Thread.CurrentThread.ManagedThreadId} IsPool: {Thread.CurrentThread.IsThreadPoolThread}");
-                // Some long running code here on dedicated thread
-                Thread.Sleep(1000);
-
-                // Return to a threadpool thread
-                await Task.Yield();
-                Console.WriteLine($"Thread4: {Thread.CurrentThread.ManagedThreadId} IsPool: {Thread.CurrentThread.IsThreadPoolThread}");
-            }, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default).Unwrap();
-
-            await t1;
-
-            Console.WriteLine($"BEFORE DELAY1: {TaskScheduler.Current.GetType().Name}");
-
-            await Task.Delay(1000).ConfigureScheduler(scheduler);
-
-            Console.WriteLine($"AFTER DELAY1: {TaskScheduler.Current.GetType().Name}");
-
-            await Task.Delay(1000);
-
-
-            Console.WriteLine($"AFTER DELAY2: {TaskScheduler.Current.GetType().Name}");
-
-            await Task.CompletedTask.ConfigureScheduler(scheduler);
-
-            Console.WriteLine($"AFTER AWAIT1: {TaskScheduler.Current.GetType().Name}");
-
-            await Task.CompletedTask.ConfigureScheduler(TaskScheduler.Default);
-
-            Console.WriteLine($"AFTER AWAIT2: {TaskScheduler.Current.GetType().Name}");
-
-            return;
-            */
+          
 
             using (var agent = AgentFactory.CreateCoordinatorAgent(agentOptions))
             {
